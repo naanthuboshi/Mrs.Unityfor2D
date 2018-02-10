1192b534561b7efce796dd7e79d8c31834db258f
山口奈々 <7stars.0296@gmail.com>
2018-02-10 15:46:01 +0900

    superunity
    追加

--- Assets/UnityChan2D/Scripts/UnityChan2DController.cs
+++ Assets/UnityChan2D/Scripts/UnityChan2DController.cs
@@ -1,133 +1,52 @@
-﻿using System.Collections;
-using UnityEngine;
-[RequireComponent(typeof(Animator), typeof(Rigidbody2D), typeof(BoxCollider2D))]
-public class UnityChan2DController : MonoBehaviour
-{
-    public float maxSpeed = 10f;
-    public float jumpPower = 1000f;
-    public Vector2 backwardForce = new Vector2(-4.5f, 5.4f);
+﻿using UnityEngine;
+using System.Collections;
 
-    public LayerMask whatIsGround;
+public class UnityChanController2D : MonoBehaviour {
+	//アニメーションするためのコンポーネントを入れる
+	Animator animator;
 
-    private Animator m_animator;
-    private BoxCollider2D m_boxcollier2D;
-    private Rigidbody2D m_rigidbody2D;
-    private bool m_isGround;
-    private const float m_centerY = 1.5f;
+	//Unityちゃんを移動させるコンポーネントを入れる（追加）
+	Rigidbody2D rigid2D;
 
-    private State m_state = State.Normal;
+	// 地面の位置
+	private float groundLevel = -3.0f;
 
-    void Reset()
-    {
-        Awake();
+	// ジャンプの速度の減衰（追加）
+	private float dump = 0.8f;
 
-        // UnityChan2DController
-        maxSpeed = 10f;
-        jumpPower = 1000;
-        backwardForce = new Vector2(-4.5f, 5.4f);
-        whatIsGround = 1 << LayerMask.NameToLayer("Ground");
+	// ジャンプの速度（追加）
+	float jumpVelocity = 20;
 
-        // Transform
-        transform.localScale = new Vector3(1, 1, 1);
+	// Use this for initialization
+	void Start () {
+		// アニメータのコンポーネントを取得する
+		this.animator = GetComponent<Animator> ();
+		// Rigidbody2Dのコンポーネントを取得する（追加）
+		this.rigid2D = GetComponent<Rigidbody2D> ();
 
-        // Rigidbody2D
-        m_rigidbody2D.gravityScale = 3.5f;
-        m_rigidbody2D.fixedAngle = true;
+	}
 
-        // BoxCollider2D
-        m_boxcollier2D.size = new Vector2(1, 2.5f);
-        m_boxcollier2D.offset = new Vector2(0, -0.25f);
+	// Update is called once per frame
+	void Update () {
 
-        // Animator
-        m_animator.applyRootMotion = false;
-    }
+		// 走るアニメーションを再生するために、Animatorのパラメータを調節する
+		this.animator.SetFloat("Horizontal", 1);
 
-    void Awake()
-    {
-        m_animator = GetComponent<Animator>();
-        m_boxcollier2D = GetComponent<BoxCollider2D>();
-        m_rigidbody2D = GetComponent<Rigidbody2D>();
-    }
+		// 着地しているかどうかを調べる
+		bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+		this.animator.SetBool ("isGround", isGround);
 
-    void Update()
-    {
-        if (m_state != State.Damaged)
-        {
-            float x = Input.GetAxis("Horizontal");
-            bool jump = Input.GetButtonDown("Jump");
-            Move(x, jump);
-        }
-    }
+		// 着地状態でクリックされた場合（追加）
+		if (Input.GetMouseButtonDown (0) && isGround) {
+			// 上方向の力をかける（追加）
+			this.rigid2D.velocity = new Vector2 (0, this.jumpVelocity);
+		}
 
-    void Move(float move, bool jump)
-    {
-        if (Mathf.Abs(move) > 0)
-        {
-            Quaternion rot = transform.rotation;
-            transform.rotation = Quaternion.Euler(rot.x, Mathf.Sign(move) == 1 ? 0 : 180, rot.z);
-        }
-
-        m_rigidbody2D.velocity = new Vector2(move * maxSpeed, m_rigidbody2D.velocity.y);
-
-        m_animator.SetFloat("Horizontal", move);
-        m_animator.SetFloat("Vertical", m_rigidbody2D.velocity.y);
-        m_animator.SetBool("isGround", m_isGround);
-
-        if (jump && m_isGround)
-        {
-            m_animator.SetTrigger("Jump");
-            SendMessage("Jump", SendMessageOptions.DontRequireReceiver);
-            m_rigidbody2D.AddForce(Vector2.up * jumpPower);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        Vector2 pos = transform.position;
-        Vector2 groundCheck = new Vector2(pos.x, pos.y - (m_centerY * transform.localScale.y));
-        Vector2 groundArea = new Vector2(m_boxcollier2D.size.x * 0.49f, 0.05f);
-
-        m_isGround = Physics2D.OverlapArea(groundCheck + groundArea, groundCheck - groundArea, whatIsGround);
-        m_animator.SetBool("isGround", m_isGround);
-    }
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.tag == "DamageObject" && m_state == State.Normal)
-        {
-            m_state = State.Damaged;
-            StartCoroutine(INTERNAL_OnDamage());
-        }
-    }
-
-    IEnumerator INTERNAL_OnDamage()
-    {
-        m_animator.Play(m_isGround ? "Damage" : "AirDamage");
-        m_animator.Play("Idle");
-
-        SendMessage("OnDamage", SendMessageOptions.DontRequireReceiver);
-
-        m_rigidbody2D.velocity = new Vector2(transform.right.x * backwardForce.x, transform.up.y * backwardForce.y);
-
-        yield return new WaitForSeconds(.2f);
-
-        while (m_isGround == false)
-        {
-            yield return new WaitForFixedUpdate();
-        }
-        m_animator.SetTrigger("Invincible Mode");
-        m_state = State.Invincible;
-    }
-
-    void OnFinishedInvincibleMode()
-    {
-        m_state = State.Normal;
-    }
-
-    enum State
-    {
-        Normal,
-        Damaged,
-        Invincible,
-    }
+		// クリックをやめたら上方向への速度を減速する（追加）
+		if (Input.GetMouseButton (0) == false) {
+			if (this.rigid2D.velocity.y > 0) {
+				this.rigid2D.velocity *= this.dump;
+			}
+		}
+	}
 }
